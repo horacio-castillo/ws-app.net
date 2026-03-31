f775711571f0a0efce69be010a228cbfacbff0db
Horacio Castillo Dev <hcastillo@pinmasadelanto.com>
2026-03-30 22:51:56 -0600

    Commit Initial

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ws-app.net
+
+Swagger
+https://localhost:7280/swagger
+
+Scalar
+https://localhost:7280/scalar/v1
---
+++ wsApp/Program.cs
@@ -0,0 +1,22 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
